275b171abf13ece4458e5e79656748805622d4ec
Miguel Saldana <mig.saldana@gmail.com>
2018-05-01 23:38:16 -0500

    Implement edit article feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
 
   protected
-  def resource_not_found
-  end
+
+  def resource_not_found; end
+
 end
